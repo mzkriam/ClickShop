7474343cc9042ebde36f45997f21e8974bc33520
mzkrim <maraizkaria4@gmail.com>
2025-03-24 00:22:04 +0300

    WebSite

--- resources/views/WebSite/header.blade.php
+++ resources/views/WebSite/header.blade.php
@@ -4,43 +4,42 @@
 </div>
 <!-- Spinner End -->
 
-
 <!-- Navbar start -->
 <div class="container-fluid fixed-top">
     <div class="container topbar bg-primary d-none d-lg-block">
         <div class="d-flex justify-content-between">
             <div class="top-info ps-2">
-                <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">123 Street, New York</a></small>
-                <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">Email@Example.com</a></small>
+                <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">{{ trans('Website/Website.address') }}</a></small>
+                <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">ClickShop@gmail.com</a></small>
             </div>
             <div class="top-link pe-2">
-                <a href="#" class="text-white"><small class="text-white mx-2">Privacy Policy</small>/</a>
-                <a href="#" class="text-white"><small class="text-white mx-2">Terms of Use</small>/</a>
-                <a href="#" class="text-white"><small class="text-white ms-2">Sales and Refunds</small></a>
+                <a href="#" class="text-white"><small class="text-white mx-2">{{ trans('Website/Website.privacy_policy') }}</small>/</a>
+                <a href="#" class="text-white"><small class="text-white mx-2">{{ trans('Website/Website.terms_of_use') }}</small>/</a>
+                <a href="#" class="text-white"><small class="text-white ms-2">{{ trans('Website/Website.sales') }}</small></a>
             </div>
         </div>
     </div>
     <div class="container px-0">
         <nav class="navbar navbar-light bg-white navbar-expand-xl">
-            <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">Fruitables</h1></a>
+            <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">{{ trans('Website/Website.name_website') }}</h1></a>
             <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars text-primary"></span>
             </button>
             <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                 <div class="navbar-nav mx-auto">
-                    <a href="index.html" class="nav-item nav-link active">Home</a>
-                    <a href="shop.html" class="nav-item nav-link">Shop</a>
-                    <a href="shop-detail.html" class="nav-item nav-link">Shop Detail</a>
+                    <a href="index.html" class="nav-item nav-link active">{{ trans('Website/Website.home') }}</a>
+                    <a href="shop.html" class="nav-item nav-link">{{ trans('Website/Website.shop') }}</a>
+                    <a href="shop-detail.html" class="nav-item nav-link">{{ trans('Website/Website.shop_detail') }}</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">{{ trans('Website/Website.pages') }}</a>
                         <div class="dropdown-menu m-0 bg-secondary rounded-0">
-                            <a href="{{ asset('WebSite/cart.blade') }}" class="dropdown-item">Cart</a>
-                            <a href="blank.blade" class="dropdown-item">Chackout</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="404.html" class="dropdown-item">404 Page</a>
+                            <a href="{{ asset('WebSite/cart.blade') }}" class="dropdown-item">{{ trans('Website/Website.cart') }}</a>
+                            <a href="blank.blade" class="dropdown-item">{{ trans('Website/Website.checkout') }}</a>
+                            <a href="testimonial.html" class="dropdown-item">{{ trans('Website/Website.testimonial') }}</a>
+                            <a href="404.html" class="dropdown-item">{{ trans('Website/Website.404_page') }}</a>
                         </div>
                     </div>
-                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    <a href="contact.html" class="nav-item nav-link">{{ trans('Website/Website.contact') }}</a>
                 </div>
                 <div class="d-flex m-3 me-0">
                     <button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
@@ -48,7 +47,7 @@
                         <i class="fa fa-shopping-bag fa-2x"></i>
                         <span class="position-absolute bg-secondary rounded-circle d-flex align-items-center justify-content-center text-dark px-1" style="top: -5px; left: 15px; height: 20px; min-width: 20px;">3</span>
                     </a>
-                    <a href="{{route('admin.dashboard')}}" title="Log In" class="my-auto">
+                    <a href="{{route('admin.dashboard')}}" title="{{ trans('Website/Website.login') }}" class="my-auto">
                         <i class="fas fa-user fa-2x"></i>
                     </a>
                 </div>
@@ -57,5 +56,3 @@
     </div>
 </div>
 <!-- Navbar End -->
-
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,12 @@
     <div class="modal-dialog modal-fullscreen">
         <div class="modal-content rounded-0">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Search by keyword</h5>
+                <h5 class="modal-title" id="exampleModalLabel">{{ trans('Website/Website.search_by_keyword') }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body d-flex align-items-center">
                 <div class="input-group w-75 mx-auto d-flex">
-                    <input type="search" class="form-control p-3" placeholder="keywords" aria-describedby="search-icon-1">
+                    <input type="search" class="form-control p-3" placeholder="{{ trans('Website/Website.search_placeholder') }}" aria-describedby="search-icon-1">
                     <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
                 </div>
             </div>
@@ -20,38 +20,43 @@
 </div>
 <!-- Modal Search End -->
 
-  <!-- Hero Start -->
+<!-- Hero Start -->
 <div class="container-fluid py-5 mb-5 hero-header">
     <div class="container py-5">
         <div class="row g-5 align-items-center">
             <div class="col-md-12 col-lg-7">
-                <h4 class="mb-3 text-secondary">100% Organic Foods</h4>
-                <h1 class="mb-5 display-3 text-primary">Organic Veggies & Fruits Foods</h1>
+                <h4 class="mb-3 text-secondary">{{ trans('Website/Website.open_market') }}</h4>
+                <h1 class="mb-5 display-3 text-primary">{{ trans('Website/Website.shop_now') }}</h1>
                 <div class="position-relative mx-auto">
-                    <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="number" placeholder="Search">
-                    <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Submit Now</button>
+                    <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="number" placeholder="{{ trans('Website/Website.search_placeholder') }}">
+                    <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">{{ trans('Website/Website.submit_now') }}</button>
                 </div>
             </div>
             <div class="col-md-12 col-lg-5">
                 <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
                     <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active rounded">
-                            <img src="{{asset('WebSite/assets/img/hero-img-1.png')}}" class="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide">
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Fruites</a>
+                            <img src="{{asset('WebSite/assets/img/landing_3.jpg')}}" class="img-fluid w-100 h-100 bg-secondary rounded" alt="First slide" style="object-fit: cover;">
+                            <a href="#" class="btn px-4 py-2 text-white rounded">{{ trans('Website/Website.products') }}</a>
                         </div>
                         <div class="carousel-item rounded">
-                            <img src="{{asset('WebSite/assets/img/hero-img-2.jpg')}}" class="img-fluid w-100 h-100 rounded" alt="Second slide">
-                            <a href="#" class="btn px-4 py-2 text-white rounded">Vesitables</a>
+                            <img src="{{asset('WebSite/assets/img/landing_5.jpg')}}" class="img-fluid w-100 h-100 rounded" alt="Second slide" style="object-fit: cover;">
+                            <a href="#" class="btn px-4 py-2 text-white rounded">{{ trans('Website/Website.products') }}</a>
+                        </div>                        
+                        <div class="carousel-item rounded">
+                            <img src="{{asset('WebSite/assets/img/landing_4.jpg')}}" class="img-fluid w-100 h-100 rounded" alt="Fourth slide" style="object-fit: cover;">
+                            <a href="#" class="btn px-4 py-2 text-white rounded">{{ trans('Website/Website.sell_now') }}</a>
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                    
+                    {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
-                    </button>
+                    </button> --}}
                 </div>
             </div>
         </div>
@@ -67,44 +72,44 @@
             <div class="col-md-6 col-lg-3">
                 <div class="featurs-item text-center rounded bg-light p-4">
                     <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
-                        <i class="fas fa-car-side fa-3x text-white"></i>
+                        <i class="fas fa-truck fa-3x text-white"></i>
                     </div>
                     <div class="featurs-content text-center">
-                        <h5>{{ trans('Dashboard/category.category') }}</h5>
-                        <p class="mb-0">Free on order over $300</p>
+                        <h5>{{ trans('Website/Website.shipping') }}</h5>
+                        <p class="mb-0">{{ trans('Website/Website.shipping_info') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="featurs-item text-center rounded bg-light p-4">
                     <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
-                        <i class="fas fa-user-shield fa-3x text-white"></i>
+                        <i class="fas fa-lock fa-3x text-white"></i>
                     </div>
                     <div class="featurs-content text-center">
-                        <h5>Security Payment</h5>
-                        <p class="mb-0">100% security payment</p>
+                        <h5>{{ trans('Website/Website.security_payment') }}</h5>
+                        <p class="mb-0">{{ trans('Website/Website.security_payment_info') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="featurs-item text-center rounded bg-light p-4">
                     <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
-                        <i class="fas fa-exchange-alt fa-3x text-white"></i>
+                        <i class="fas fa-gift fa-3x text-white"></i>
                     </div>
                     <div class="featurs-content text-center">
-                        <h5>30 Day Return</h5>
-                        <p class="mb-0">30 day money guarantee</p>
+                        <h5>{{ trans('Website/Website.exclusive_offers') }}</h5>
+                        <p class="mb-0">{{ trans('Website/Website.exclusive_offers_info') }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3">
                 <div class="featurs-item text-center rounded bg-light p-4">
                     <div class="featurs-icon btn-square rounded-circle bg-secondary mb-5 mx-auto">
-                        <i class="fa fa-phone-alt fa-3x text-white"></i>
+                        <i class="fa fa-headset fa-3x text-white"></i>
                     </div>
                     <div class="featurs-content text-center">
-                        <h5>24/7 Support</h5>
-                        <p class="mb-0">Support every time fast</p>
+                        <h5>{{ trans('Website/Website.support') }}</h5>
+                        <p class="mb-0">{{ trans('Website/Website.support_info') }}</p>
                     </div>
                 </div>
             </div>
@@ -112,7 +117,6 @@
     </div>
 </div>
 <!-- Featurs Section End -->
-
 
 <!-- Fruits Shop Start-->
 <div class="container-fluid fruite py-5">
